66598599c1456da84417f2e474a3c76f741d58d2
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-09 12:54:56 -0500

    Initial Commit

---
+++ ViewTheStorm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ViewTheStorm
+//
+//  Created by Dan Lindsay on 2017-02-09.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ViewTheStorm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ViewTheStorm
+//
+//  Created by Dan Lindsay on 2017-02-09.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
