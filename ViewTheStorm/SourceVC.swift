20c3e9d5c875b5af5f25336aa604df48bd191ca3
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-09 14:26:06 -0500

    Setup SourceVC to conform to NSTableViewDelegate and DataSource.

--- ViewTheStorm/SourceVC.swift
+++ ViewTheStorm/SourceVC.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class SourceVC: NSViewController {
+class SourceVC: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
     
     
     @IBOutlet var tableView: NSTableView!
@@ -16,6 +16,20 @@ class SourceVC: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.make(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        
+        vw.textField?.stringValue = "Hello world!"
+        
+        return vw
     }
     
 }
