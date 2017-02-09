93f3f6770473ba9d84ce154e5eab2d7b9e073ce0
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-09 13:19:39 -0500

    Created SourceVC and DetailVC and set them up in Storyboard

---
+++ ViewTheStorm/DetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  DetailVC.swift
+//  ViewTheStorm
+//
+//  Created by Dan Lindsay on 2017-02-09.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Cocoa
+
+class DetailVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ ViewTheStorm/SourceVC.swift
@@ -0,0 +1,18 @@
+//
+//  SourceVC.swift
+//  ViewTheStorm
+//
+//  Created by Dan Lindsay on 2017-02-09.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Cocoa
+
+class SourceVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
