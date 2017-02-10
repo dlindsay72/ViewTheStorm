28fef60767c65dc375bea82122f2235532fb48f9
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-10 12:52:30 -0500

    added toolbar with sharing functionality

--- ViewTheStorm/ViewController.swift
+++ ViewTheStorm/ViewController.swift
@@ -8,19 +8,22 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSSplitViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
+    
+    
 
 
 }
